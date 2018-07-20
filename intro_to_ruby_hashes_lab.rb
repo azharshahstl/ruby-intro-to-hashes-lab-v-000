b299d620f1cf6508b5ca4a16d978c8951a42b3be
Azhar Shah <azharshahstl@yahoo.com>
2018-07-20 21:58:05 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,12 +12,10 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}}
-	
 end
 
 def monopoly_with_third_tier
-  
-		
+  	monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}}
 end
 
 def monopoly_with_fourth_tier
